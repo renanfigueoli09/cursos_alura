5c1795ade89283f9a29f820637dd22c672cb368c
renanfigueoli <renanfigueoli@outlook.com>
2022-10-25 19:10:34 -0300

    alfood-aula-6

---
+++ alfood-aula-6/src/App.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from 'react-router-dom';
+import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
+import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
+import FormularioPrato from './paginas/Administracao/Pratos/FormularioPrato';
+import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
+import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
+import Home from './paginas/Home';
+import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+
+      <Route path='/admin' element={<PaginaBaseAdmin />}>
+
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+
+        <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
+      </Route>
+
+    </Routes>
+  );
+}
+
+export default App;
