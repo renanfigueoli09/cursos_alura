f12255c7b1ccb77d846489f485ceb8fb35e12a44
renanfigueoli <renanfigueoli@outlook.com>
2022-10-25 19:22:40 -0300

    remove jquery-modulo-1-final

--- jquery-modulo-1-final/alura-typer/servidor/README.md
+++
@@ -1,3 +0,0 @@
-# alura-typer
-Mini project: A type racer like game
-
--- jquery-modulo-1-final/alura-typer/servidor/app/routes/frases.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-
-	var api = app.api.frases;
-
-	app.route('/frases/')
-		.get(api.lista);
-};
