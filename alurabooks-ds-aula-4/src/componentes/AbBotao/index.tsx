3b69cbcd3cb5301b51941e950e83b508732cc702
renanfigueoli09 <renanfigueoli@outlook.com>
2023-06-21 23:28:58 -0300

    alurabooks-ds-aula-4

---
+++ alurabooks-ds-aula-4/src/componentes/AbBotao/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+export interface AbBotaoProps {
+    texto?: string
+    tipo?: 'primario' | 'secundario'
+    onClick?: () => void
+}
+
+const BotaoEstilizado = styled.button<AbBotaoProps>`
+    background: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#EB9B00' : '#FFF'};
+    padding: 16px 32px;
+    border: 2px solid #EB9B00;
+    color: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#FFF' : '#EB9B00'};
+    font-size: 20px;
+    cursor: pointer;
+    ${(props: AbBotaoProps) => props.tipo === 'primario' 
+        ? css`
+            &:hover {
+                background: #B87900;
+                border: 2px solid #B87900;
+            }
+        `
+        : css`
+        &:hover {
+            background: #FFF;
+            border: 2px solid #B87900;
+            color: #B87900;
+        }
+        `
+    }
+`
+export const AbBotao = ({ texto, onClick, tipo = 'primario' } : AbBotaoProps) => {
+    return (<BotaoEstilizado onClick={onClick} tipo={tipo}>
+        {texto}
+    </BotaoEstilizado>)
+}
+
+
---
+++ alurabooks-ds-aula-4/stories/AbBotao.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { AbBotao, AbBotaoProps } from '../src'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+export default {
+    title: 'Componentes/AbBotao',
+    component: AbBotao
+} as ComponentMeta<typeof AbBotao>
+
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args}/>
+
+export const Primario = Template.bind({})
+
+Primario.args = {
+    texto: 'Ab Botão Primário',
+    tipo: 'primario'
+} as AbBotaoProps
+
+export const Secundario = Template.bind({})
+
+Secundario.args = {
+    texto: 'Ab Botão Secundário',
+    tipo: 'secundario'
+} as AbBotaoProps
